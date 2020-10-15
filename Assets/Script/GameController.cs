200a2ff799d8ee0fd3b8f8714fd2e03187b0c523
YonHoshihara <hoshiharyon@gmail.com>
2020-10-15 09:28:36 -0400

    Add 3D Model and ARobject

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject [] table_elements; 
+    void Start()
+    {
+        generate_sequence(5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public List<int> generate_sequence(int sequence_size) {
+        
+        List<int> sequence  = new List<int>();
+        float generated_number = 0;
+        int table_elements_lengh = table_elements.Length;
+        for (int i=0; i<=sequence_size+1; i++){
+            generated_number =Random.Range(0, table_elements_lengh);
+            
+            Debug.Log(generated_number);
+            //sequence.Add(generated_number);   
+        }
+       
+        return sequence;
+    }
+
+}
